700218f1eaa3e1e59c06aae51ec107bc3b8dd022
MahdMalik <mahdmahdmalik@gmail.com>
2024-11-17 02:07:43 -0600

    Deez Who

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,5 @@
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
-const { PinataSDK } = require('pinata');
-
 
 const createToken = (_id) => {
   return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
@@ -29,23 +27,6 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password)
-
-    const pinata = new PinataSDK({
-      pinataJwt: process.env.VITE_PINATA_JWT,
-      pinataGateway: process.env.VITE_GATEWAY_URL
-    })
-    const group = await pinata.groups.create({
-      name: email,
-    });
-    // const response = await fetch("https://" + pinata.pinataGateway + "/ipfs/" + process.env.QmTp2hEo8eXRp6wg7jXv1BLCMh5a4F3B7buAUZNZUu772j",
-    //   {headers:
-    //   {
-    //     'x-pinata-gateway-token': 'Qsml9dIG8OVv8wDHmO3oY022BaZlnCzUrdOBuWwaAxMV-dAzfQBUp-L-29v2osVQ'
-    //   }}
-    // )
-    
-
-
     // create a token
     const token = createToken(user._id)
     res.status(200).json({email, token})
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -21,7 +21,7 @@ userSchema.statics.signup = async function(email, password) {
 
   // validation
   if (!email || !password) {
-    throw Error('All fields must be filled')
+    throw Error('All fields must be filled' + email + ", " + password)
   }
   if (!validator.isEmail(email)) {
     throw Error('Email not valid')
